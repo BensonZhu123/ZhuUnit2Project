a8e1ba4ad626ec7c5c5eebcfedf32f2ec70422ad
student <williamh29@nycstudents.net>
2022-10-18 12:14:36 -0400

    push1

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+    public LinearEquation(int x1, int y1, int x2, int y2){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.y2 = y2;
+        this.x2 = x2;
+    }
+    public double distance(){
+        return roundedToHundredth(Math.pow((Math.pow(x2-x1,2) + Math.pow(y2-y1,2)),0.5));
+    }
+
+    public double yIntercept(){
+        return roundedToHundredth(y1-(slope()*x1));
+    }
+
+    public double slope(){
+        double slope = (y2-y1)/(x2-x1);
+        return roundedToHundredth(slope);
+    }
+
+    public String equation() {
+
+
+
+        }
+
+    public String coordinateForX(double xValue){
+        return "";
+    }
+
+    public double roundedToHundredth(double toRound){
+        double round = Math.round(toRound*100);
+        return round/100;
+    }
+
+    public String lineInfo(){
+        return "";
+    }
+
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class LinearEquationRunner {
+import java.util.Scanner;
+public class LinearEquationRunner {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter coordinate 1: ");
+        String coor1 = scan.nextLine();
+        int len1 = coor1.length();
+        int x1 = Integer.parseInt(coor1.substring(1, coor1.indexOf(",")));
+        int y1 = Integer.parseInt(coor1.substring(coor1.indexOf(",")+2, len1-1));
+        System.out.print("Enter coordinate 2: ");
+        String coor2 = scan.nextLine();
+        int len2 = coor2.length();
+        int x2 = Integer.parseInt(coor2.substring(1, coor2.indexOf(",")));
+        int y2 = Integer.parseInt(coor2.substring(coor2.indexOf(",")+2, len2-1));
+        LinearEquation eq1 = new LinearEquation(x1,y1,x2,y2);
+
+
+    }
 }
